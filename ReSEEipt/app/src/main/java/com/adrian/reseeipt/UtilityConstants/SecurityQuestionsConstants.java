26e3127c90b2040c939e811f926e562e76a8dd6b
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-12 16:49:18 +0800

    finished security questions array constants

--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/UtilityConstants/SecurityQuestionsConstants.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/UtilityConstants/SecurityQuestionsConstants.java
@@ -1,5 +1,27 @@
 package com.adrian.reseeipt.UtilityConstants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class SecurityQuestionsConstants {
-    //public static final String SAMPLE = "What is your sample?";
+
+    // Array containing questions
+    private static final String[] questions = {
+            "What is the name of your first pet?",
+            "Where did your parents meet?",
+            "What school did you first attend?",
+            "What is your mother's maiden name?",
+            "What is your oldest sibling's second name?",
+            "What was your first cellphone number?",
+            "What was your first telephone number?",
+            "What day of the week were you born?",
+            "What hour of the day were you born?",
+            "What is your first car?"
+    };
+
+    // Return unmodifiable list to prevent editing
+    public static ArrayList<String> getQuestions(){
+        return new ArrayList<String>(Collections.unmodifiableList(Arrays.asList(questions)));
+    }
 }
