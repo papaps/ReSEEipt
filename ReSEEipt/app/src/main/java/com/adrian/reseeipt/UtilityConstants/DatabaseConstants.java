c1414ccbb5f2c5a6efb0ebfb46eeb5867bc42a5e
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-12 17:24:23 +0800

    created database constants

---
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/UtilityConstants/DatabaseConstants.java
@@ -0,0 +1,33 @@
+package com.adrian.reseeipt.UtilityConstants;
+
+public class DatabaseConstants {
+    // Database Related Items
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "reseept_db";
+
+    //Users table constants
+    public static final String USERS_TABLE_NAME = "users";
+    public static final String USERS_KEY_ID = "user_id";
+    public static final String USERS_KEY_FIRST = "first_name";
+    public static final String USERS_KEY_LAST = "last_name";
+    public static final String USERS_KEY_PASSWORD = "password";
+    public static final String USERS_KEY_Q1 = "question1";
+    public static final String USERS_KEY_ANS1 = "answer1";
+    public static final String USERS_KEY_Q2 = "question2";
+    public static final String USERS_KEY_ANS2 = "answer2";
+
+    //Receipts table constants
+    public static final String RECEIPTS_TABLE_NAME = "receipts";
+    public static final String RECEIPT_KEY_ID = "receipt_id";
+    public static final String RECEIPT_KEY_TITLE = "title";
+    public static final String RECEIPT_KEY_NOTES = "notes";
+    public static final String RECEIPT_KEY_CATEGORY = "category";
+    public static final String RECEIPT_KEY_DATE = "date";
+
+    //Receipt images table constants
+    public static final String IMAGES_TABLE_NAME = "receipt_images";
+    public static final String IMAGES_KEY_ID = "image_id";
+    public static final String IMAGES_KEY_RECEIPT = "receipt_id";
+    public static final String IMAGES_KEY_BYTES = "image_bytes";
+
+}
