97661a5057aaef99a8bc93e7a2b3959411ca7d26
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-12 20:09:40 +0800

    created receipt image pojo

---
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/Model/ReceiptImage.java
@@ -0,0 +1,44 @@
+package com.adrian.reseeipt.Model;
+
+public class ReceiptImage {
+    private int imageID;
+    private int receiptID;
+    private byte[] imageBytes;
+
+    // Use this constructor when retrieving objects from database and converting it into objects
+    public ReceiptImage(int imageID, int receiptID, byte[] imageBytes) {
+        this.imageID = imageID;
+        this.receiptID = receiptID;
+        this.imageBytes = imageBytes;
+    }
+
+    // Use this constructor when adding new image objects
+    public ReceiptImage(int receiptID, byte[] imageBytes) {
+        this.receiptID = receiptID;
+        this.imageBytes = imageBytes;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public int getReceiptID() {
+        return receiptID;
+    }
+
+    public void setReceiptID(int receiptID) {
+        this.receiptID = receiptID;
+    }
+
+    public byte[] getImageBytes() {
+        return imageBytes;
+    }
+
+    public void setImageBytes(byte[] imageBytes) {
+        this.imageBytes = imageBytes;
+    }
+}
