a7939013215c69e6dd2d4968f2aee1a4de598a1d
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-12 20:52:51 +0800

    added images arraylist in receipt

--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/Model/Receipt.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/Model/Receipt.java
@@ -1,27 +1,32 @@
 package com.adrian.reseeipt.Model;
 
+import java.util.ArrayList;
+
 public class Receipt {
     private int receiptID;
     private String title;
     private String notes;
     private String categories;
     private String dateAdded;
+    private ArrayList<ReceiptImage> images = new ArrayList<>();
 
     // Use this constructor when retrieving objects from database and converting it into objects
-    public Receipt(int receiptID, String title, String notes, String categories, String dateAdded) {
+    public Receipt(int receiptID, String title, String notes, String categories, String dateAdded, ArrayList<ReceiptImage> images) {
         this.receiptID = receiptID;
         this.title = title;
         this.notes = notes;
         this.categories = categories;
         this.dateAdded = dateAdded;
+        this.images = images;
     }
 
     // Use this constructor when adding new image objects
-    public Receipt(String title, String notes, String categories, String dateAdded) {
+    public Receipt(String title, String notes, String categories, String dateAdded, ArrayList<ReceiptImage> images) {
         this.title = title;
         this.notes = notes;
         this.categories = categories;
         this.dateAdded = dateAdded;
+        this.images = images;
     }
 
     public int getReceiptID() {
@@ -62,5 +67,13 @@ public class Receipt {
 
     public void setDateAdded(String dateAdded) {
         this.dateAdded = dateAdded;
+    }
+
+    public ArrayList<ReceiptImage> getImages() {
+        return new ArrayList<ReceiptImage>(images);
+    }
+
+    public void setImages(ArrayList<ReceiptImage> images) {
+        this.images = images;
     }
 }
