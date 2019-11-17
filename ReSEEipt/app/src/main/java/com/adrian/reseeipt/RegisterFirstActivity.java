f5a6e57a29e15510023a0350a03a13570065dc14
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-17 17:14:28 +0800

    temporary reguster page added

---
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/RegisterFirstActivity.java
@@ -0,0 +1,14 @@
+package com.adrian.reseeipt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterFirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_first);
+    }
+}
