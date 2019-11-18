bcaad75cfeb106e722b7b0daf28f49ac0e40a1f0
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-18 20:19:07 +0800

    working ish register first activity

--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/Constants/ReceiptCategoryConstants.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/Constants/ReceiptCategoryConstants.java
@@ -1,11 +1,21 @@
 package com.adrian.reseeipt.Constants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ReceiptCategoryConstants {
-    public static final String GROCERIES = "Groceries";
-    public static final String SHOPPING = "Shopping";
-    public static final String MEAL = "Meal";
-    public static final String UTILITY = "Utility";
-    public static final String GADGETS = "Gadgets";
-    public static final String APPLIANCES = "Appliances";
-    public static final String OTHERS = "Others";
+    public static final String[] CATEGORIES= {"Groceries",
+    "Shopping",
+    "Meal",
+    "Utility",
+    "Gadgets",
+    "Appliances",
+    "Others"
+    };
+
+    // Return unmodifiable list to prevent editing
+    public static ArrayList<String> getCategories(){
+        return new ArrayList<String>(Collections.unmodifiableList(Arrays.asList(CATEGORIES)));
+    }
 }
--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/RegisterFirstActivity.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/RegisterFirstActivity.java
@@ -2,15 +2,19 @@ package com.adrian.reseeipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterFirstActivity extends AppCompatActivity {
     private EditText firstNameField;
     private EditText lastNameField;
     private EditText passwordField;
     private EditText confirmPasswordField;
+    private TextView registerFirstErrorText;
     private Button registerFirstCancelButton;
     private Button registerFirstNextButton;
 
@@ -20,7 +24,53 @@ public class RegisterFirstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_first);
 
         firstNameField = findViewById(R.id.firstNameField);
+        lastNameField = findViewById(R.id.lastNameField);
+        passwordField = findViewById(R.id.passwordField);
+        confirmPasswordField = findViewById(R.id.confirmPasswordField);
+        registerFirstCancelButton = findViewById(R.id.registerFirstCancelButton);
+        registerFirstNextButton = findViewById(R.id.registerFirstNextButton);
+        registerFirstErrorText = findViewById(R.id.registerFirstErrorTextLabel);
+
+        registerFirstNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String errorMessage = validateFields();
+                if (errorMessage.equals("Okay")){
+                    //Todo Put the Stuff in intent
+                    //Todo Go to next part of Register
+                    Intent intent = new Intent(RegisterFirstActivity.this, RegisterSecurityActivity.class);
+
+                    startActivity(intent);
+                    finish();
+                } else {
+                    registerFirstErrorText.setText(errorMessage);
+                }
+            }
+        });
+
+        registerFirstCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterFirstActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private String validateFields(){
+        String answer = "Okay";
 
 
+        if (firstNameField.getText().toString().isEmpty() ||
+            lastNameField.getText().toString().isEmpty() ||
+            passwordField.getText().toString().isEmpty() ||
+            confirmPasswordField.getText().toString().isEmpty()){
+            answer = "Missing Fields";
+        } else if (!passwordField.getText().toString().equals(confirmPasswordField.getText().toString())){
+            answer = "Passwords Do Not Match";
+        }
+
+        return answer;
     }
 }
