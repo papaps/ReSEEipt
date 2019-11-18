92a792742b40eb9d8f9d7725b4a22ce43d9fcbeb
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-18 17:51:50 +0800

    working assess if something exists

---
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/Constants/SharedPrefConstants.java
@@ -0,0 +1,6 @@
+package com.adrian.reseeipt.Constants;
+
+public class SharedPrefConstants {
+    public static final String APP_PREFERENCE_NAME = "reseept_prefs";
+    public static final String PREF_USER_ID = "user_id";
+}
