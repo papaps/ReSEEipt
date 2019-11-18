88945903ef03005a7a4ff393369d8031ee200457
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-19 00:41:27 +0800

    working log in and all

--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/Database/DatabaseHandler.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/Database/DatabaseHandler.java
@@ -26,37 +26,37 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         // Create the user table
         String CREATE_USER_TABLE = "CREATE TABLE " + DatabaseConstants.USERS_TABLE_NAME +
-                "(" +
+                " ( " +
                 DatabaseConstants.USERS_KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                DatabaseConstants.USERS_KEY_FIRST + "TEXT NOT NULL, " +
-                DatabaseConstants.USERS_KEY_LAST + "TEXT NOT NULL, " +
-                DatabaseConstants.USERS_KEY_PASSWORD + "TEXT NOT NULL, " +
-                DatabaseConstants.USERS_KEY_Q1 + "TEXT NOT NULL, " +
-                DatabaseConstants.USERS_KEY_ANS1 + "TEXT NOT NULL, " +
-                DatabaseConstants.USERS_KEY_Q2 + "TEXT NOT NULL, " +
-                DatabaseConstants.USERS_KEY_ANS2 + "TEXT NOT NULL " +
-                ")";
+                DatabaseConstants.USERS_KEY_FIRST + " TEXT NOT NULL, " +
+                DatabaseConstants.USERS_KEY_LAST + " TEXT NOT NULL, " +
+                DatabaseConstants.USERS_KEY_PASSWORD + " TEXT NOT NULL, " +
+                DatabaseConstants.USERS_KEY_Q1 + " TEXT NOT NULL, " +
+                DatabaseConstants.USERS_KEY_ANS1 + " TEXT NOT NULL, " +
+                DatabaseConstants.USERS_KEY_Q2 + " TEXT NOT NULL, " +
+                DatabaseConstants.USERS_KEY_ANS2 + " TEXT NOT NULL " +
+                " );";
         db.execSQL(CREATE_USER_TABLE);
 
         // Create the receipts table
         String CREATE_RECEIPT_TABLE = "CREATE TABLE " + DatabaseConstants.RECEIPTS_TABLE_NAME +
-                "(" +
+                " ( " +
                 DatabaseConstants.RECEIPTS_KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 DatabaseConstants.RECEIPTS_KEY_TITLE + " TEXT NOT NULL, " +
                 DatabaseConstants.RECEIPTS_KEY_NOTES+ " TEXT NOT NULL, " +
                 DatabaseConstants.RECEIPTS_KEY_CATEGORY + " TEXT NOT NULL, " +
                 DatabaseConstants.RECEIPTS_KEY_DATE + " TEXT NOT NULL " +
-                ")";
+                " );";
         db.execSQL(CREATE_RECEIPT_TABLE);
 
         // Create the receipts images table
         // https://stackoverflow.com/questions/11790104/how-to-storebitmap-image-and-retrieve-image-from-sqlite-database-in-android
         String CREATE_RECEIPT_IMAGE_TABLE = "CREATE TABLE " + DatabaseConstants.IMAGES_TABLE_NAME +
-                "(" +
+                " ( " +
                 DatabaseConstants.IMAGES_KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 DatabaseConstants.IMAGES_KEY_RECEIPT + " TEXT NOT NULL, " +
                 DatabaseConstants.IMAGES_KEY_BYTES+ " BLOB " +
-                ")";
+                " ); ";
         db.execSQL(CREATE_RECEIPT_IMAGE_TABLE);
     }
 
@@ -274,7 +274,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         ArrayList<Receipt> list = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
 
-        String selectAll = "SELECT * FROM " + DatabaseConstants.RECEIPTS_TABLE_NAME + " WHERE " + DatabaseConstants.RECEIPTS_KEY_CATEGORY + " = " + category;
+        String selectAll = "SELECT * FROM " + DatabaseConstants.RECEIPTS_TABLE_NAME + " WHERE " + DatabaseConstants.RECEIPTS_KEY_CATEGORY + " = '" + category + "'";
         Cursor cursor = db.rawQuery(selectAll, null);
 
         if (cursor.moveToFirst()) {
--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/LoginActivity.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/LoginActivity.java
@@ -2,13 +2,79 @@ package com.adrian.reseeipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.adrian.reseeipt.Constants.IntentConstants;
+import com.adrian.reseeipt.Constants.SharedPrefConstants;
+import com.adrian.reseeipt.Database.DatabaseHandler;
+import com.adrian.reseeipt.Model.User;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView welcomeNameText;
+    private EditText passwordLoginField;
+    private TextView forgotPasswordText;
+    private Button loginContinueButton;
+    private TextView loginErrorText;
+
+    SharedPreferences sharedPreferences;
+    private DatabaseHandler databaseHandler;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        databaseHandler = new DatabaseHandler(this);
+        sharedPreferences = getSharedPreferences(SharedPrefConstants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE);
+
+        // Get Stored User ID
+        int user_id = sharedPreferences.getInt(SharedPrefConstants.PREF_USER_ID, 1);
+        user = databaseHandler.getUser(user_id);
+
+        // Set up the thingies to be used.
+        welcomeNameText = findViewById(R.id.welcomeNameText);
+        passwordLoginField = findViewById(R.id.passwordLoginField);
+        forgotPasswordText = findViewById(R.id.forgotPasswordText);
+        loginContinueButton = findViewById(R.id.loginContinueButton);
+        loginErrorText = findViewById(R.id.loginErrorText);
+
+        // Set the name of the welcome text
+        welcomeNameText.setText(user.getFirstName() + " " + user.getLastName() + ".");
+
+        loginContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String errorMessage = validateFields();
+                if (errorMessage.equals("Okay")){
+                    //Todo Put the Stuff in intent and call activity
+                    Toast.makeText(getApplicationContext(), user.getAnswer1(), Toast.LENGTH_LONG).show();
+                } else {
+                    loginErrorText.setText(errorMessage);
+                }
+            }
+        });
+    }
+
+    private String validateFields(){
+        String answer = "Okay";
+
+
+        if (passwordLoginField.getText().toString().isEmpty()){
+            answer = "Please Input Your Password";
+        } else if (passwordLoginField.getText().toString().equals(user.getPassword())){
+            answer = "Passwords is Incorrect";
+        }
+
+        return answer;
     }
 }
