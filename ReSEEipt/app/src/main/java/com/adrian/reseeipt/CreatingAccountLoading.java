efcd28671cc7f462b97e6d5c26015ecdc64f110e
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-11-18 23:10:34 +0800

    added loading creating screen gif

---
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/CreatingAccountLoading.java
@@ -0,0 +1,14 @@
+package com.adrian.reseeipt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatingAccountLoading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating_account_loading);
+    }
+}
