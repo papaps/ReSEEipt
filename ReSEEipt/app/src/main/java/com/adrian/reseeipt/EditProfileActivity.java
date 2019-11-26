641d6878172aefb6a3139f35b58a37789be1d866
schuyl3r <schuyler_ng@dlsu.edu.ph>
2019-11-26 18:09:12 +0800

    edit profile works

--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/EditProfileActivity.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/EditProfileActivity.java
@@ -2,13 +2,84 @@ package com.adrian.reseeipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.adrian.reseeipt.Constants.SharedPrefConstants;
+import com.adrian.reseeipt.Database.DatabaseHandler;
+import com.adrian.reseeipt.Model.User;
+import com.bumptech.glide.disklrucache.DiskLruCache;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    EditText FirstNameField, LastNameField;
+    TextView messageField;
+    Button EditCancelButton, DeleteConfirmButton;
+
+    SharedPreferences sharedPreferences;
+    private DatabaseHandler databaseHandler;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        FirstNameField = findViewById(R.id.FirstNameField);
+        LastNameField = findViewById(R.id.LastNameField);
+        messageField = findViewById(R.id.messageField);
+        EditCancelButton = findViewById(R.id.EditCancelButton);
+        DeleteConfirmButton = findViewById(R.id.DeleteConfirmButton);
+
+        databaseHandler = new DatabaseHandler(this);
+        sharedPreferences = getSharedPreferences(SharedPrefConstants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE);
+
+        // Get Stored User ID
+        int user_id = sharedPreferences.getInt(SharedPrefConstants.PREF_USER_ID, 1);
+        user = databaseHandler.getUser(user_id);
+
+        EditCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntent = new Intent(EditProfileActivity.this, ProfileActivity.class);
+                startActivity(newIntent);
+                finish();
+            }
+        });
+
+        DeleteConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String errorMessage = validateFields();
+                if (errorMessage.equals("Okay")){
+                    user.setFirstName(FirstNameField.getText().toString());
+                    user.setLastName(LastNameField.getText().toString());
+                    databaseHandler.updateUser(user);
+                    Intent newIntent = new Intent(EditProfileActivity.this, ProfileActivity.class);
+                    startActivity(newIntent);
+                    finish();
+                } else {
+                    messageField.setText(errorMessage);
+                }
+            }
+        });
+    }
+
+    private String validateFields(){
+        String answer = "Okay";
+
+        if (FirstNameField.getText().toString().matches("")){
+            answer = "Please Input Your First Name";
+        } else if (!LastNameField.getText().toString().matches("")){
+            answer = "Please Input Your Last Name";
+        }
+
+        return answer;
     }
 }
--- ReSEEipt/app/src/main/java/com/adrian/reseeipt/ProfileActivity.java
+++ ReSEEipt/app/src/main/java/com/adrian/reseeipt/ProfileActivity.java
@@ -2,23 +2,73 @@ package com.adrian.reseeipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.adrian.reseeipt.Constants.SharedPrefConstants;
+import com.adrian.reseeipt.Database.DatabaseHandler;
+import com.adrian.reseeipt.Model.User;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ImageButton profileReturnButton;
+    private TextView FirstNameField, LastNameField;
+    private LinearLayout edit_linearlayout, delete_linearlayout;
+
+    SharedPreferences sharedPreferences;
+    private DatabaseHandler databaseHandler;
+    private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
         profileReturnButton = findViewById(R.id.ProfileReturnButton);
+        FirstNameField = findViewById(R.id.FirstNameField);
+        LastNameField = findViewById(R.id.LastNameField);
+        edit_linearlayout = findViewById(R.id.edit_linearlayout);
+        delete_linearlayout = findViewById(R.id.delete_linearlayout);
+
+        databaseHandler = new DatabaseHandler(this);
+        sharedPreferences = getSharedPreferences(SharedPrefConstants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE);
+
+        // Get Stored User ID
+        int user_id = sharedPreferences.getInt(SharedPrefConstants.PREF_USER_ID, 1);
+        user = databaseHandler.getUser(user_id);
+
+        FirstNameField.setText(user.getFirstName());
+        LastNameField.setText(user.getLastName());
 
         profileReturnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
+            }
+        });
+
+        edit_linearlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(newIntent);
+                finish();
+            }
+        });
+
+        delete_linearlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseHandler.deleteUser(user);
+                getSharedPreferences(SharedPrefConstants.PREF_USER_ID, 0).edit().clear().commit();
+                Intent newIntent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(newIntent);
                 finish();
             }
         });
